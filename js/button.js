ca59d1d0ab4cd2f3b023e180ec199a37a6c6881d
Basil <ionart3737@gmail.com>
2018-06-27 15:14:56 +0400

    adding files

---
+++ js/button.js
@@ -0,0 +1,15 @@
+window.onload =function () {
+$(window).scroll(function() {
+    if ($(this).scrollTop()) {
+        $('#toTop').fadeIn();
+    } else {
+        $('#toTop').fadeOut();
+    }
+});
+
+$("#toTop").click(function() {
+    $("html, body").animate({scrollTop: 0}, 1000);
+ });	
+}
+
+
